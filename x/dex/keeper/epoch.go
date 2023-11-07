e86192cf40699dfff69e46d2a9b7f75d9cac6f4a
Jeremy Wei <jeremy.t.wei@gmail.com>
2023-11-07 10:45:48 -0500

    make a change to cause chain to panic after a while

--- x/dex/keeper/epoch.go
+++ x/dex/keeper/epoch.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	"encoding/binary"
 	"fmt"
+	"math/rand"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -15,6 +16,14 @@ func (k Keeper) SetEpoch(ctx sdk.Context, epoch uint64) {
 	binary.BigEndian.PutUint64(bz, epoch)
 	store.Set([]byte(EpochKey), bz)
 	ctx.Logger().Info(fmt.Sprintf("Current epoch %d", epoch))
+
+	// try to cause a panic after block height 1000
+	if ctx.BlockHeight() >= 1000 {
+		// set a random number
+		randNum := rand.Intn(10000)
+		byteArray := make([]byte, 4)
+		binary.BigEndian.PutUint32(byteArray, uint32(randNum))
+	}
 }
 
 func (k Keeper) IsNewEpoch(ctx sdk.Context) (bool, uint64) {
